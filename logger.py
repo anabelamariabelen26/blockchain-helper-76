5439217c148527e6e61e43d5e180c83fc8d10607
Anabela Mariabelen <anabelamariabelen5429@hotmail.com>
2026-08-26 07:14:18 +0200

    add logger setup with rotation

---
+++ logger.py
@@ -0,0 +1,28 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+def setup_logger(name: str = "blockchain_helper") -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    
+    if logger.handlers:
+        return logger
+        
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    
+    file_handler = RotatingFileHandler(
+        "blockchain_helper.log",
+        maxBytes=10 * 1024 * 1024,
+        backupCount=5
+    )
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    
+    return logger
